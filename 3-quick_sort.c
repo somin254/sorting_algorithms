3679cad958412ef0337bcc538de9ada8065f2c96
somin254 <sominrenson@gmail.com>
2024-02-23 12:26:13 -0800

    function that sorts an array of integer

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -2,55 +2,60 @@
 
 /**
  * custom_swap - swaps two integers in an array
- * @first_num: pointer to the first element
- * @second_num: pointer to the second element
+ * @first_element: pointer to the first element
+ * @second_element: pointer to the second element
  */
-void custom_swap(int *first_num, int *second_num) {
-    int temp = *first_num;
-    *first_num = *second_num;
-    *second_num = temp;
+void custom_swap(int *first_element, int *second_element) {
+    int temp = *first_element;
+    *first_element = *second_element;
+    *second_element = temp;
 }
 
 /**
  * custom_partition - partitions the array using Lomuto scheme
  * @arr: input array
- * @low: index of the first element
- * @high: index of the last element
+ * @left_index: index of the first element
+ * @right_index: index of the last element
  * @size: size of the array
  * Return: integer representing the pivot position
  */
-int custom_partition(int *arr, int low, int high, size_t size) {
-    int pivot = arr[high];
-    int i = (low - 1);
+int custom_partition(int *arr, int left_index, int right_index, size_t size) {
+    int pivot_value;
+    int i = left_index;
 
-    for (int j = low; j <= high; j++) {
-        if (arr[j] <= pivot) {
-            i++;
+    pivot_value = arr[right_index];
+
+    for (int j = left_index; j < right_index; j++) {
+        if (arr[j] <= pivot_value) {
             custom_swap(&arr[i], &arr[j]);
             if (i != j)
                 print_array(arr, size);
+            i++;
         }
     }
 
-    custom_swap(&arr[i + 1], &arr[high]);
-    if (i != high)
+    custom_swap(&arr[i], &arr[right_index]);
+    if (i != right_index) {
         print_array(arr, size);
+    }
 
-    return (i + 1);
+    return i;
 }
 
 /**
  * quick_sort_array - sorts the array using QuickSort algorithm
  * @arr: input array
- * @low: index of the first element
- * @high: index of the last element
+ * @left_index: index of the first element
+ * @right_index: index of the last element
  * @size: size of the array
  */
-void quick_sort_array(int *arr, int low, int high, size_t size) {
-    if (low < high) {
-        int pivot = custom_partition(arr, low, high, size);
-        quick_sort_array(arr, low, pivot - 1, size);
-        quick_sort_array(arr, pivot + 1, high, size);
+void quick_sort_array(int *arr, int left_index, int right_index, size_t size) {
+    int pivot;
+
+    if (left_index < right_index) {
+        pivot = custom_partition(arr, left_index, right_index, size);
+        quick_sort_array(arr, left_index, pivot - 1, size);
+        quick_sort_array(arr, pivot + 1, right_index, size);
     }
 }
 
@@ -60,8 +65,8 @@ void quick_sort_array(int *arr, int low, int high, size_t size) {
  * @size: size of the array
  */
 void quick_sort(int *array, size_t size) {
-    if (size < 2)
+    if (size < 2) {
         return;
-
+    }
     quick_sort_array(array, 0, size - 1, size);
 }
