d0f5ea1b258f6e9417a9d3d9bbce08ed58cb622b
fordstile <masterland9772@gmail.com>
2024-02-25 07:40:54 -0800

    updated

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -5,10 +5,11 @@
  * @first: pointer to the first element
  * @second: pointer to the second element
  */
-void swap_elements(int *first, int *second) {
-    int temp = *first;
-    *first = *second;
-    *second = temp;
+void swap_elements(int *first, int *second)
+{
+	int temp = *first;
+	*first = *second;
+	*second = temp;
 }
 
 /**
@@ -16,27 +17,32 @@ void swap_elements(int *first, int *second) {
  * @array: the array to be sorted
  * @size: size of the array
  */
-void selection_sort(int *array, size_t size) {
-    unsigned int outer_index = 0, inner_index = 0, min_loc = 0;
-    int min_value;
+void selection_sort(int *array, size_t size)
+{
+	unsigned int outer_index = 0, inner_index = 0, min_loc = 0;
+	int min_value;
 
-    while (outer_index < size) {
-        inner_index = outer_index;
-        min_value = array[inner_index];
-        min_loc = inner_index;
+	while (outer_index < size)
+	{
+		inner_index = outer_index;
+		min_value = array[inner_index];
+		min_loc = inner_index;
 
-        while (inner_index < size) {
-            if (array[inner_index] < min_value) {
-                min_value = array[inner_index];
-                min_loc = inner_index;
-            }
-            inner_index++;
-        }
+		while (inner_index < size)
+		{
+			if (array[inner_index] < min_value)
+			{
+				min_value = array[inner_index];
+				min_loc = inner_index;
+			}
+			inner_index++;
+		}
 
-        if (array[outer_index] != array[min_loc]) {
-            swap_elements(array + outer_index, array + min_loc);
-            print_array(array, size);
-        }
-        outer_index++;
-    }
+		if (array[outer_index] != array[min_loc])
+		{
+			swap_elements(array + outer_index, array + min_loc);
+			print_array(array, size);
+		}
+		outer_index++;
+	}
 }
